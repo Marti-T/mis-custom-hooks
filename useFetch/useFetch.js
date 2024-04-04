3d27d38bf6641ef07f25ce036bea59124f3dbb63
Marti-T <64131196+Marti-T@users.noreply.github.com>
2024-04-04 21:31:11 +0200

    Update useFetch.js

--- useFetch/useFetch.js
+++ useFetch/useFetch.js
@@ -1,49 +1,83 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useState } from "react";
+
+
+const localCache = {}
+
 
 export const useFetch = ( url ) => {
 
-    const idMounted = useRef(true);
-    const [state, setState] = useState({ data: null, loading: true, error: null });
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError: false,
+        error: null,
+    });
 
-    useEffect( () => {
+    useEffect(() => {
 
-        return () => {
-            //idMounted.current = true;
-            idMounted.current = false;
+        getFetch();
+
+    }, [url]);
+
+    const setLoadingState = () => {
+        setState({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            error: null,
+        });
+    }
+
+    const getFetch = async() => {
+
+        if ( localCache[url] ) {
+            console.log("Usando caché");
+            setState({
+                data: localCache[url],
+                isLoading: false,
+                hasError: false,
+                error: null,
+            });
+            return;
         }
 
-    }, [] );
-    
+        setLoadingState();
 
-    useEffect( () => {
+        const resp = await fetch(url);
 
-        setState({ data: null, loading: true, error: null });
+        // sleep
+        await new Promise( resolve => setTimeout(resolve, 1500));
 
-        fetch( url )
-        .then( resp => resp.json() )
-        .then( data => {
-
-            if ( idMounted.current ) {
-                setState({
-                    loading: false,
-                    error: null,
-                    data
-                })
-            } else {
-                //console.log('setState no se llamó');
-            }
-
-        })
-        .catch( () => {
+        if(!resp.ok) {
             setState({
                 data: null,
-                loading: false,
-                error: 'No se pudo cargar la info'
-            })
-        })
-        
-    },[url])
+                isLoading: false,
+                hasError: true,
+                error: {
+                    code: resp.status,
+                    message: resp.statusText
+                }
+            });
+            return;
+        }
 
-    return state;
-    
+        const data = await resp.json();
+
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            error: null,
+        });
+
+        // Manejo del caché
+        localCache[url] = data;
+
+    }
+
+    return {
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError
+    }
 }
